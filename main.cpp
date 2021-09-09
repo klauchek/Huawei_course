8d75c5ac0275634bed3c38124d7fe2dfc26ccec8
klauchek <kniazeva.ms@phystech.edu>
2021-09-09 19:45:48 +0300

    some documentation added

--- main.cpp
+++ main.cpp
@@ -16,19 +16,20 @@ int nRoots = SolveSquare (a, b, c, &x1, &x2);
 
 switch (nRoots)
 {
-    case ZERO_ROOTS: printf("No roots\n");
-            break;
-
-    case ONE_ROOT: printf("x = %lg\n", x1);
-            break;
-
-    case TWO_ROOTS: printf("x1 = %lg, x2 = %lg\n", x1, x2);
-            break;
-    case INF_ROOTS: printf("Any number\n");
-
-    default: printf ("main(): ERROR: Incorrect amount of roots: %d\n", nRoots);
-
-            return 1;
+        case ZERO_ROOTS: printf("No roots\n");
+                break;
+            
+        case ONE_ROOT: printf("x = %lg\n", x1);
+                break;
+            
+        case TWO_ROOTS: printf("x1 = %lg, x2 = %lg\n", x1, x2);\
+                break;
+            
+        case INF_ROOTS: printf("Any number\n");
+                break;
+                
+        default: printf ("main(): ERROR: Incorrect amount of roots: %d\n", nRoots);
+                return 1;
 }
 
 
--- square_eq.cpp
+++ square_eq.cpp
@@ -1,14 +1,20 @@
 #include "square_eq.h"
 
-inline bool Compare (double a, double b)
-{
-    return (fabs(a - b) < EPSILON);
-}
-
-inline double CalcDiscriminant (double a, double b, double c)
-{
-    return b * b - 4 * a * c;
-}
+//---------------------------------------------------------------------
+//! Solves a square equation ax^2 + bx + c = 0  
+//! @param [in]  a   a‐coefficient 
+//! @param [in]  b   b‐coefficient 
+//! @param [in]  c   c‐coefficient 
+//! @param [out] x1  Pointer to the 1st root 
+//! @param [out] x2  Pointer to the 2nd root
+//! 
+//! @return Number of roots
+//!
+//! @note   Returns INF_ROOTS if equation has infinity amount of roots
+//!                 ZERO_ROOTS if equation has no roots in real numbers
+//!                 ONE_ROOT if equation has one root
+//!                 TWO_ROOTS if equation has two roots
+//----------------------------------------------------------------------
 
 int SolveSquare(double a, double b, double c, double *x1, double *x2)
 {
@@ -55,6 +61,36 @@ int SolveSquare(double a, double b, double c, double *x1, double *x2)
             return ZERO_ROOTS;
     }
 
+}
+
+
+//------------------------------------------
+//! Compares two numbers
+//! @param [in] a   first number
+//! @param [in] b   second number
+//!
+//! @return Returns 1 if numbers are equal
+//!                 0 otherwise
+//!
+//! @note   Accuracy is defined by EPSILON constant
+//------------------------------------------
+
+inline bool Compare (double a, double b)
+{
+    return (fabs(a - b) < EPSILON);
+}
+
+//------------------------------------------
+//! Calculates discriminant
+//! @param [in]  a   a‐coefficient 
+//! @param [in]  b   b‐coefficient 
+//! @param [in]  c   c‐coefficient 
+//!
+//! @return discriminant
+//------------------------------------------
+inline double CalcDiscriminant (double a, double b, double c)
+{
+    return b * b - 4 * a * c;
 }
 
 
